c6de7b88b47500c45e94bfa05b5f10346a2b64d3
Yash Jain <yash.jain106@gmail.com>
2022-08-09 22:48:15 +0530

    Delete Python 3.x/Upgrad/Modules/Module 1 directory

--- Python 3.x/Upgrad/Modules/Module 1/Session 2 - Data Structures in Python/Adding Python to Output.py
+++
@@ -1,7 +0,0 @@
-# Adding element Python to an input tuple 
-
-input_str=input('Enter your input in a comma-separated way: ');
-input_list=input_str.split(',')+['Python'];
-
-tuple2=tuple(input_list);
-print('The final tuple is: ',tuple2);
--- Python 3.x/Upgrad/Modules/Module 1/Session 3 - Control Structures/Title Case Lists.py
+++
@@ -1,8 +0,0 @@
-#Title case lists
-
-input_list=['list1','LIST2','LIsT3','lISt4'];
-tc_list=[];
-for i in input_list:
-    tc_list+=[i[0].upper()+i[1:].lower()];
-print(tc_list)
-    
